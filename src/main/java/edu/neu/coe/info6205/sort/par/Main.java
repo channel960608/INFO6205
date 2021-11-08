c9c9fc20af2dc7e65783940b15ea78708dd1b8c0
channel960608 <997476274@qq.com>
2021-11-08 00:55:15 -0500

    FEATURE: assignment5

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -1,89 +1,125 @@
 package edu.neu.coe.info6205.sort.par;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.ForkJoinPool;
+import edu.neu.coe.info6205.util.Benchmark_Timer;
+import edu.neu.coe.info6205.util.LazyLogger;
+import edu.neu.coe.info6205.util.WriterUtils;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
 
 /**
- * This code has been fleshed out by Ziyao Qiao. Thanks very much.
- * TODO tidy it up a bit.
  */
 public class Main {
 
+    final static LazyLogger logger = new LazyLogger(Main.class);
+
+    static int stepCutoff = 2;
+    static int stepParallelism = 2;
+    static int stepSize = 2;
+
+    static int baseParallelism = 1;
+    static int baseCutoff = 1 << 12;
+    static int baseSize = 1 << 18;
+
     public static void main(String[] args) {
-        processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
-        Random random = new Random();
-        int[] array = new int[2000000];
-        ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
-            // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-            long time;
-            long startTime = System.currentTimeMillis();
-            for (int t = 0; t < 10; t++) {
-                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-                ParSort.sort(array, 0, array.length);
+        configure(args);
+        doExperiments();
+    }
+
+    private static void configure(String[] args) {
+        Arrays.stream(args).forEach(Main::processArg);
+    }
+
+    private static void processArg(String str) {
+        if (str.startsWith("-step") || str.startsWith("-base")) {
+            try {
+                String[] keyValue = str.split("=");
+                switch (keyValue[0]) {
+                    case "-stepCutoff":
+                        stepCutoff = Integer.parseInt(keyValue[1]);
+                        break;
+                    case "-stepParallelism":
+                        stepParallelism = Integer.parseInt(keyValue[1]);
+                        break;
+                    case "-stepSize":
+                        stepSize = Integer.parseInt(keyValue[1]);
+                        break;
+                    case "-baseCutoff":
+                        baseCutoff = Integer.parseInt(keyValue[1]);
+                        break;
+                    case "-baseParallelism":
+                        baseParallelism = Integer.parseInt(keyValue[1]);
+                        break;
+                    case "-baseSize":
+                        baseSize = Integer.parseInt(keyValue[1]);
+                        break;
+                    default:
+                        throw new Exception();
+                }
+            } catch (Exception e) {
+                logger.warn("Unable to process argument " + str);
             }
-            long endTime = System.currentTimeMillis();
-            time = (endTime - startTime);
-            timeList.add(time);
-
-
-            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
-
         }
-        try {
-            FileOutputStream fis = new FileOutputStream("./src/result.csv");
-            OutputStreamWriter isr = new OutputStreamWriter(fis);
-            BufferedWriter bw = new BufferedWriter(isr);
-            int j = 0;
-            for (long i : timeList) {
-                String content = (double) 10000 * (j + 1) / 2000000 + "," + (double) i / 10 + "\n";
-                j++;
-                bw.write(content);
-                bw.flush();
+    }
+
+    public static void doExperiments() {
+
+
+        double[][][] times = new double[stepParallelism][stepCutoff][stepSize];
+        int curCutoff, curParallelism, curSize;
+        for (int par = 0; par < stepParallelism; ++par) {
+            curParallelism = baseParallelism << par;
+            for (int cutoff = 0; cutoff < stepCutoff; ++cutoff) {
+                curCutoff = baseCutoff << cutoff;
+                for (int size = 0; size < stepSize; ++size) {
+                    curSize = baseSize << size;
+                    double t = doOneExperiment(curCutoff, curSize, curParallelism);
+                    times[par][cutoff][size] = t;
+                }
             }
-            bw.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < stepParallelism * stepCutoff * stepSize; ++i) {
+            sb.append(i);
+            sb.append(", ");
+            sb.append(baseParallelism << (i / (stepCutoff * stepSize)));
+            sb.append(", ");
+            sb.append(baseCutoff << (i % (stepCutoff * stepSize) / stepSize));
+            sb.append(", ");
+            sb.append(baseSize << (i % stepSize));
+            sb.append(", ");
+            sb.append(times[i / (stepCutoff * stepSize)][i % (stepCutoff * stepSize) / stepSize][i % stepSize]);
+            sb.append("\n");
+        }
+        WriterUtils.write(String.format("./result%s.csv", LocalDateTime.now()), sb.toString());
     }
 
-    private static void processArgs(String[] args) {
-        String[] xs = args;
-        while (xs.length > 0)
-            if (xs[0].startsWith("-")) xs = processArg(xs);
+    public static double doOneExperiment(int cutoff, int arraySize) {
+        return doOneExperiment(cutoff, arraySize, 1);
     }
 
-    private static String[] processArg(String[] xs) {
-        String[] result = new String[0];
-        System.arraycopy(xs, 2, result, 0, xs.length - 2);
-        processCommand(xs[0], xs[1]);
-        return result;
+    public static double doOneExperiment(int cutoff, int arraySize, int parallelism) {
+        logger.info(String.format("Start to sort array with cutoff = %d, arraySize = %d, parallelism = %d"
+                , cutoff, arraySize, parallelism));
+        final double t = new Benchmark_Timer<ParSort>(
+                "InsertSortBenchmarkReversed",
+                ps -> {
+                    ps.init();
+                    return ps;
+                },
+                ps -> {
+                    ps.sortArray();
+                },
+                (rs) -> {
+                    return;
+                }
+        ).runFromSupplier(() -> new ParSort(cutoff, arraySize, parallelism), 10);
+        logger.info(
+                String.format("Complete sorting in mean time %fms with cutoff = %d, arraySize = %d, parallelism = %d"
+                , t, cutoff, arraySize,parallelism)
+        );
+        return t;
     }
-
-    private static void processCommand(String x, String y) {
-        if (x.equalsIgnoreCase("N")) setConfig(x, Integer.parseInt(y));
-        else
-            // TODO sort this out
-            if (x.equalsIgnoreCase("P")) //noinspection ResultOfMethodCallIgnored
-                ForkJoinPool.getCommonPoolParallelism();
-    }
-
-    private static void setConfig(String x, int i) {
-        configuration.put(x, i);
-    }
-
-    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
-    private static final Map<String, Integer> configuration = new HashMap<>();
-
-
 }
--- src/main/java/edu/neu/coe/info6205/sort/par/ParSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/ParSort.java
@@ -1,7 +1,9 @@
 package edu.neu.coe.info6205.sort.par;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
 
 /**
  * This code has been fleshed out by Ziyao Qiao. Thanks very much.
@@ -9,17 +11,55 @@ import java.util.concurrent.CompletableFuture;
  */
 class ParSort {
 
-    public static int cutoff = 1000;
+    private int cutoff = 1000;
+    private int arraySize = 2000000;
+    private int parallelism = 1;
+    private int[] array;
+    private ForkJoinPool pool;
+    private Random random = new Random();
 
-    public static void sort(int[] array, int from, int to) {
+    public int getCutoff() {
+        return cutoff;
+    }
+
+    public void setCutoff(int cutoff) {
+        this.cutoff = cutoff;
+    }
+
+    public int getArraySize() {
+        return arraySize;
+    }
+
+    public void setArraySize(int arraySize) {
+        this.arraySize = arraySize;
+    }
+
+    public ParSort(int cutoff, int arraySize, int parallelism) {
+        this.cutoff = cutoff;
+        this.arraySize = arraySize;
+        this.parallelism = parallelism;
+    }
+
+    public void init() {
+        this.array = new int[getArraySize()];
+        int range = Integer.MAX_VALUE / 5 > getArraySize() ? Integer.MAX_VALUE : 5 * getArraySize();
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = random.nextInt(range);
+        }
+        this.pool = new ForkJoinPool(parallelism);
+    }
+
+    public void sortArray() {
+        sort(array, 0, getArraySize());
+    }
+
+    public void sort(int[] array, int from, int to) {
         if (to - from < cutoff) Arrays.sort(array, from, to);
         else {
-            // FIXME next few lines should be removed from public repo.
             CompletableFuture<int[]> parsort1 = parsort(array, from, from + (to - from) / 2); // TO IMPLEMENT
             CompletableFuture<int[]> parsort2 = parsort(array, from + (to - from) / 2, to); // TO IMPLEMENT
             CompletableFuture<int[]> parsort = parsort1.thenCombine(parsort2, (xs1, xs2) -> {
                 int[] result = new int[xs1.length + xs2.length];
-                // TO IMPLEMENT
                 int i = 0;
                 int j = 0;
                 for (int k = 0; k < result.length; k++) {
@@ -37,20 +77,18 @@ class ParSort {
             });
 
             parsort.whenComplete((result, throwable) -> System.arraycopy(result, 0, array, from, result.length));
-//            System.out.println("# threads: "+ ForkJoinPool.commonPool().getRunningThreadCount());
             parsort.join();
         }
     }
 
-    private static CompletableFuture<int[]> parsort(int[] array, int from, int to) {
+    private CompletableFuture<int[]> parsort(int[] array, int from, int to) {
         return CompletableFuture.supplyAsync(
                 () -> {
                     int[] result = new int[to - from];
-                    // TO IMPLEMENT
                     System.arraycopy(array, from, result, 0, result.length);
                     sort(result, 0, to - from);
                     return result;
-                }
+                }, this.pool
         );
     }
 }
